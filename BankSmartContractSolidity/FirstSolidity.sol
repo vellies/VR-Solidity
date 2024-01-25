7f1aca4bbc88534ca83a158e8bc402728911d867
vellies <vellies6113@gmail.com>
2024-01-25 11:35:49 +0530

    Solidity Variables

--- BankSmartContractSolidity/FirstSolidity.sol
+++ BankSmartContractSolidity/FirstSolidity.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.8;
 
 contract FirstSolidity {
 
-    uint public BankBalance = 50; //Static Bank balance
+    uint256 public BankBalance = 50;
+    bool public OverDraftFlag = true;
+    string public AccountName = "Vellieswaran R";
+    int public NumberOfTransactions = 10;
+    address public MetaMaskWallet;
+    bytes32 AccountEmail = "vellies@yopmail.com";
 
 }
