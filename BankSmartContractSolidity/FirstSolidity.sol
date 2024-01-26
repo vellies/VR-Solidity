a3aadf829a28d326715a079e1d46ac62386959b1
vellies <vellies6113@gmail.com>
2024-01-26 08:31:37 +0530

    Structs & Arrays

--- BankSmartContractSolidity/FirstSolidity.sol
+++ BankSmartContractSolidity/FirstSolidity.sol
@@ -3,6 +3,7 @@
 pragma solidity 0.8.8;
 
 contract FirstSolidity {
+    // Storage Variables --> That can modified
     uint256 public BankBalance = 50;
     bool OverDraftFlag = true;
     string AccountName = "Vellieswaran R";
@@ -21,5 +22,29 @@ contract FirstSolidity {
 
     function getNewBankBalance() public view returns (uint256) {
         return BankBalance;
+    }
+
+    // Struct & Arrays
+    // Struct is have multiple datatypes.
+    struct Transaction {
+        string FromName;
+        uint256 Amount;
+    }
+
+    Transaction public TransactionStruct =
+        Transaction({FromName: "Vellies", Amount: 25});
+    Transaction TransactionStruct1 =
+        Transaction({FromName: "Ramani", Amount: 50});
+    Transaction TransactionStruct2 = Transaction({FromName: "VR", Amount: 100});
+
+    Transaction[] public TransactionList;
+
+    // Storage Functions - 3 Types
+    // Call data, Memory, Storage (calldata,memory,storage)
+    // calldata --> Temp Variable, can't change it.
+    // memory --> Temp Variable, can change.
+    // storage --> Default and permanent variable.
+    function addTransaction(string memory _fromName, uint256 _amount) public {
+        TransactionList.push(Transaction(_fromName, _amount));
     }
 }
